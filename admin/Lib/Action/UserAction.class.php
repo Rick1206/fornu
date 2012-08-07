6ee94682e9b6a0f4aa69e8c8032a74c37ab64ed4
rick <rickshy1206@gmail.com>
2012-08-08 00:15:45 +0800

    update_0808_01

---
+++ admin/Lib/Action/UserAction.class.php
@@ -0,0 +1,9 @@
+<?php
+// 本类由系统自动生成，仅供测试用途
+class UserAction extends Action {
+    public function add(){
+        header("Content-Type:text/html; charset=utf-8"); 
+        echo "rick";
+		$this->display(); 
+	}
+}

28757eedd914064d020668e7103c3a9e6517cf9a
rick <rickshy1206@gmail.com>
2012-08-08 01:35:24 +0800

    update_0808_03

---
+++ Public/Js/form.js
@@ -0,0 +1,247 @@
+$(function()
+{
+
+//regexp
+var reg = {
+	mobile:/^1[3|4|5|8][0-9]\d{8}$/,
+	tel:/^(\d{3,4}\d{7,8})$/,
+	emailstandard:/^[a-zA-Z0-9-._]+@[a-zA-Z0-9-._]+.[a-z]{2,3}$/,
+	email:/@/,
+	hostname:/^[a-zA-Z0-9-._]+.[a-z]{2,3}$/,
+	hostnamefirst:/^[a-zA-Z0-9-._]+$/,
+	empty:/^\S+$/,
+	pass:/^.{6,18}$/,
+	comparepass:function(){
+			
+	},
+	numberextend:function(n,n2){
+		if(n2 && n)
+		{
+			if(n && reg.number.test(n) && reg.number.test(n2))
+			{
+				var _reg = new RegExp("^\\d{"+n+","+n2+"}$");
+				return _reg;
+			}
+		}
+		else if(n)
+		{
+			if(n && reg.number.test(n))
+			{
+				var _reg = new RegExp("^\\d{"+n+"}$");
+				return _reg;
+			}	
+		}else
+		{
+			return /^\d{1,}$/;
+		}
+	},
+	nexdrule:/^[d]\d{1,}$/,
+	nexdruleextend:/^[d]\d{1,}-\d{1,}$/,
+	number:/^\d{1,}$/,
+	birth:/^[1-2][0-9]\d{2}-(([0][1-9])|([1][0-2]))-([0-2][0-9])|([3][0-1])$/,
+	year:/^[1-2][0-9]\d{2}$/,
+	month:/^([0][1-9])|([1][0-2])$/,
+	day:/^([0-2][0-9])|([3][0-1])$/,
+	cnandnotnull:/^[\u4E00-\u9FA5\x00-\xff]+$/g,
+	enandnotnull:/^[a-zA-Z]+$/,
+	zipcode:/^[1-9][0-9]{5}$/,
+	fnRegs:function($list){			
+		var $list = $($list),len = $list.length,i=0,_temp="";
+		for(;i<len;i++)
+		{
+			_temp = checkOne($list.eq(i));
+			if(_temp != "ok")
+			{
+				return "wrong";
+			}
+		}
+		return "ok";		
+	},
+	fnReg:function($obj){		
+		var $this = $obj,tempreg = $this.attr("reg");
+		
+		if(reg.nexdrule.test(tempreg))
+		{
+			tempreg = reg.numberextend(parseInt(tempreg.replace("d","")));
+		}else if(reg.nexdruleextend.test(tempreg))
+		{
+			tempreg = tempreg.replace("d","").split("-");
+			tempreg = reg.numberextend(parseInt(tempreg[0]),parseInt(tempreg[1]));
+		}else
+		{ 
+			tempreg = reg[$this.attr("reg")] || new RegExp();
+		}
+		var result = tempreg.test($this.val());
+		if(result)
+		{
+			return "ok";	
+		}
+		else
+		{
+			return 2;	
+		}
+				
+		
+			
+	}
+};	
+
+
+$("input[reg]").each(function(){
+	var $this = $(this);				
+	$this.addClass("wrong").focus(function(){
+		var _dm = $this.attr('defaultMessage');
+		if(_dm!="")
+		{
+			start.showTip($this,_dm);
+		}
+	}).blur(function(){	
+		 checkOne($this);
+	});
+});
+
+var start = {
+	showTip:function(obj,_dm){
+		$(obj).siblings("em").html(_dm);
+	},
+	closeTip:function(obj){
+		$(obj).removeClass("wrong").siblings("em").html("");
+	}
+};
+
+function checkOne($this){			
+	  var _em = $this.attr('errorMessage');		  					
+	  var _re = reg.fnReg($this);
+	  
+	  if(_re=="ok")
+	  {
+		  checkSpecial($this);
+		    
+	  }else{
+		  if(_em!="")
+		  {
+			  start.showTip($this,_em);
+		  }					
+	  }
+}
+
+//检测所有
+function check(obj){
+	var $list = $(obj).find(".wrong");	
+ 	
+	if($list.length<1){
+	}else{
+		
+		for(var i=0,j=$list.length;i<j;i++)
+		{		
+			var _temp = reg.fnReg($list.eq(i));
+			if(_temp != "ok"){
+				start.showTip($list.eq(i),$list.eq(i).attr("errorMessage"));
+				return false;
+			}
+			else
+			{
+				 checkSpecial($list.eq(i));
+			}
+		}
+	}	
+	return "ok";	
+}
+
+
+function checkSpecial($this){
+	if($this.attr("unsamereg")!=undefined)
+	  {
+			if($this.val()!=$("#"+$this.attr("unsamereg")).val())
+			{
+				start.showTip($this,$this.attr("unsameMessage"));
+				return;
+			}
+			else
+			{
+				start.closeTip($this); 	
+			}
+	  }else
+	  { 		  
+		start.closeTip($this); 
+	  }	
+}
+
+function checkCheckBox()
+{
+	var $list = $("input[type='hidden']");
+	for(var i=0,j=$list.length;i<j;i++)
+	{		
+		var $this = $list.eq(i);
+		if($this.val()==""){
+			$this.siblings("em").html("请选择");
+			return false;
+		}
+		else
+		{
+			$this.siblings("em").html("");
+		}
+	}
+	return "ok";
+}
+
+
+//提交动作
+function submitForm(){
+	$.ajax({
+		type:'POST',
+		url:'reg_submit.php',
+		data:{mobile:v("#mobile"),nickname:v("#nickname"),email:v("#email"),password:v("#password"),mumname:v("#mumname"),
+				area:v("#area"),address:v("#address"),phone:v("#phone1")+v("#phone2"),zipcode:v("#zipcode"),
+				now:v("#now"),maga:v("#maga"),activity:v("#activity"),contact:v("#contact")
+		},
+		success:function(e){
+				var e = 1;
+				if(e=="1")
+				{
+					$.lightBox(".reg-success");
+				}
+				else
+				{
+					alert("Error!Try again!");						
+				}
+		}
+	});	
+	
+	function v(id)
+	{
+		return $(id).val();	
+	}
+}
+
+
+
+//初始化
+$("#step1 input,#step2 input").val("");
+
+$("#btn_step_1").bind('click',function(){
+	if(check("#step1")=="ok")
+	{
+		$("#step1").fadeOut(0);
+		$("#step2").fadeIn(0,function(){
+			$("#btn_step_2").bind('click',function(){
+				if(check("#step2")=="ok")
+				{
+					if(checkCheckBox()=="ok")
+					{
+						submitForm();
+					}	
+				}	
+			});	
+		});
+	}	
+	else
+	{
+		return false;
+	}
+});
+
+
+
+});
+
--- home/Lib/Action/IndexAction.class.php
+++ home/Lib/Action/IndexAction.class.php
@@ -3,6 +3,7 @@
 class IndexAction extends Action {
     public function index(){
         header("Content-Type:text/html; charset=utf-8");
-        echo '<div style="font-weight:normal;color:blue;float:left;width:345px;text-align:center;border:1px solid silver;background:#E8EFFF;padding:8px;font-size:14px;font-family:Tahoma">^_^ Hello,欢迎使用<span style="font-weight:bold;color:red">ThinkPHP</span></div>';
+        //echo '<div style="font-weight:normal;color:blue;float:left;width:345px;text-align:center;border:1px solid silver;background:#E8EFFF;padding:8px;font-size:14px;font-family:Tahoma">^_^ Hello,欢迎使用<span style="font-weight:bold;color:red">ThinkPHP</span></div>';
+		$this->display();
     }
 }
